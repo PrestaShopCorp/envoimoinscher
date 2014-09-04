ae204532fe2a3dc919876f0833807928d0435af9
Thomas Nabord <thomas.nabord@prestashop.com>
2014-09-04 18:37:48 +0200

    // Brought back code with fix for sql update

--- upgrade/Upgrade-3.1.5.php
+++ upgrade/Upgrade-3.1.5.php
@@ -6,14 +6,25 @@
 function upgrade_module_3_1_5($module)
 {
 	// Execute the SQL upgrade
-	if (Db::getInstance()->execute(Tools::file_get_contents('Upgrade-3.1.5.sql')) === false)
+	$sql_file = Tools::file_get_contents('Upgrade-3.1.5.sql');
+	$sql_file = str_replace('{PREFIXE}', _DB_PREFIX_, $sql_file);
+	
+	// Because any merchant can't execute every sql queries in one execute, we have to explode them.
+	$query = explode('-- REQUEST --', $sql_file);
+	
+	Db::getInstance()->execute('START TRANSACTION;');
+	foreach ($query as $q)
 	{
-		return false;
+		if (trim($q) != '' && Db::getInstance()->execute($q) === false)
+		{
+			Db::getInstance()->execute('ROLLBACK;');
+			return false;
+		}
 	}
 	
-	// Add hooks if necessary
-	
 	// Validate upgrade
+	Db::getInstance()->execute('COMMIT;');
 	return true;
 }
+
 ?>
--- upgrade/Upgrade-3.1.5.sql
+++ upgrade/Upgrade-3.1.5.sql
@@ -1,2 +1,3 @@
 select * from `{PREFIXE}emc_operators`; -- some requests just to make sure the updater works
+-- REQUEST --
 select * from `{PREFIXE}emc_services`;
